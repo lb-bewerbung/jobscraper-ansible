7dca64b0df74cebe21cdd5bd53b1047bcfd00610
lb-bewerbung <bewerbung@lb.net>
2024-11-26 17:00:25 +0100

    added install_ansible.sh

---
+++ install_ansible.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Install ansible on control machine
+
+sudo apt update
+sudo apt install software-properties-common
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible
